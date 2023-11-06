35fbaed1baccada3131add3a4c1a54ce5060811c
Chandu <dulamchandu13@gmail.com>
2023-11-06 23:21:19 +0530

    temperature converter

---
+++ javaInfo/01-temp/01temperature.js
@@ -0,0 +1,12 @@
+let result = confirm(
+  "press ok to convert Fahrenheit to Celsius or press cancel to convert Celsius to Fahrenheit"
+);
+if (result === true) {
+  let fahrenheit = prompt("Enter Fahrenheit", [0]);
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  alert(`celsius equivalent of ${fahrenheit} fahrenheit is ${celsius}`);
+} else {
+  let celsius = prompt("Enter Celsius", [0]);
+  let fahrenheit = celsius * (9 / 5) + 32;
+  alert(`Fahrenheit equivalent of ${celsius} Celsius is ${fahrenheit}`);
+}
