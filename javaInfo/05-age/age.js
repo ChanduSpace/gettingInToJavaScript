1a8a068992476511117c10188ed6f2b08b25de0a
Chandu <dulamchandu13@gmail.com>
2023-11-15 23:24:16 +0530

    function

--- javaInfo/05-age/age.js
+++ javaInfo/05-age/age.js
@@ -1,2 +1,13 @@
-birthYear = prompt("Enter your birth year");
+let birthYear = prompt("Enter your birth year");
 alert(`Your age is ${2023 - Number(birthYear)}`);
+
+let a = "hello world";
+console.log(a);
+
+function check() {
+  let a = "hey guys..!";
+  console.log(a);
+}
+console.log(a);
+
+check();
